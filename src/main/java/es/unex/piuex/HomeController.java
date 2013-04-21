d69e80d70e36fad5edd7d697b52d8372ebc1c004
KokyRJ84 <KokyRJ84@gmail.com>
2013-04-21 13:40:04 +0200

    Untrack MANIFEST.MF sucessfully + comment

--- src/main/java/es/unex/piuex/HomeController.java
+++ src/main/java/es/unex/piuex/HomeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Handles requests for the application home page.
+ * Handles requests for the application home page. Home.
  */
 @Controller
 public class HomeController {
