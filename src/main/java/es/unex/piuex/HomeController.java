cfd13b17045b91911c1808d48b6e71ef27511432
Álvaro G. <yo@netbook>
2013-04-22 09:41:59 +0200

    Probando a ver si se va el MANIFEST...

--- src/main/java/es/unex/piuex/HomeController.java
+++ src/main/java/es/unex/piuex/HomeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Handles requests for the application home page. Home.
+ * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
