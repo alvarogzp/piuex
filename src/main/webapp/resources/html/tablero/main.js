2bc859588853e12fb9777c7392216f539f7fede7
Álvaro G. <yo@netbook>
2013-05-08 13:06:20 +0200

    Cambios en la tutoria

--- src/main/webapp/resources/html/tablero/main.js
+++ src/main/webapp/resources/html/tablero/main.js
@@ -7,18 +7,18 @@
 
 var tablero_t = $("#tablero").text(),
 	juego_d = $("#juego"),
-	tabla = [ "<table>" ];
+	tabla = [ "<table border='1'>" ];
 
 var filas = tablero_t.split("\n");
 
 _( filas ).each(function( tr, i ) {
 	
-	tabla.push ( "<tr>" );
+	tabla.push ( "<tr border='1'>" );
 	
 		_( tr.split("") ).each (function( td, j ) {
-			tabla.push(  "<td>" );
+			tabla.push(  "<td border='1'><span class='mover'>" );
 			tabla.push(    td   );
-			tabla.push( "</td>" );
+			tabla.push( "</span></td>" );
 		});
 	
 	tabla.push ( "</tr>" );
