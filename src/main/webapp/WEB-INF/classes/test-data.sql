95cbd2ea79898927f5bc24186e80eb1fe5dc7c4f
Álvaro G. <yo@netbook>
2013-05-21 22:16:10 +0200

    Actualizada base de datos

--- src/main/webapp/WEB-INF/classes/test-data.sql
+++ src/main/webapp/WEB-INF/classes/test-data.sql
@@ -1,5 +1,6 @@
 -- HSQLDB Test data for APALABRADOS SCHEMA
-INSERT INTO APALABRADOS.USER (ID, NAME, USERNAME,AVATAR,EMAIL,PASSWORD,LEVEL, RANK) VALUES (
+
+INSERT INTO APALABRADOS.USER (ID, NAME, USERNAME, AVATAR, EMAIL, PASSWORD, LEVEL, RANK) VALUES (
 	0,
 	'Administrador',
 	'admin',
@@ -10,57 +11,19 @@ INSERT INTO APALABRADOS.USER (ID, NAME, USERNAME,AVATAR,EMAIL,PASSWORD,LEVEL, RA
 	0
 );
 
+
 INSERT INTO APALABRADOS.USER (NAME, USERNAME, AVATAR, EMAIL, PASSWORD, LEVEL, RANK) VALUES (
-	'Ana Lucero',
-	'ana',
-	'ana.png',
-	'ana@unex.es',
-	'1234',
-	'newbie',
-	10
-);
-
-INSERT INTO APALABRADOS.USER (NAME, USERNAME,AVATAR,EMAIL,PASSWORD,LEVEL, RANK) VALUES (
-	'Pedro Baez',
-	'pedro',
-	'pedro.png',
-	'pedro@unex.es',
-	'1234',
-	'master',
-	10
-);
-
-INSERT INTO APALABRADOS.USER (NAME, USERNAME,AVATAR,EMAIL,PASSWORD,LEVEL, RANK) VALUES (
-	'Sara Garcia',
-	'sara',
-	'sara.png',
-	'sara@unex.es',
-	'1234',
-	'advanced',
-	10
-);
-
-INSERT INTO APALABRADOS.USER (NAME, USERNAME,AVATAR,EMAIL,PASSWORD,LEVEL, RANK) VALUES (
-	'Daniel Gonzalez',
-	'daniel',
-	'daniel.png',
-	'daniel@unex.es',
-	'1234',
-	'newbie',
-	10
-);
-
-INSERT INTO APALABRADOS.USER (NAME, USERNAME,AVATAR,EMAIL,PASSWORD,LEVEL, RANK) VALUES (
 	'Koky',
 	'koky',
-	'koky.png',
+	'koky.jpg',
 	'koky@unex.es',
 	'rjPIUEX13',
 	'master',
 	0
 );
 
-INSERT INTO APALABRADOS.USER (NAME, USERNAME,AVATAR,EMAIL,PASSWORD,LEVEL, RANK) VALUES (
+
+INSERT INTO APALABRADOS.USER (NAME, USERNAME, AVATAR, EMAIL, PASSWORD, LEVEL, RANK) VALUES (
 	'Alvaro',
 	'alvaro',
 	'alvaro.png',
@@ -71,31 +34,86 @@ INSERT INTO APALABRADOS.USER (NAME, USERNAME,AVATAR,EMAIL,PASSWORD,LEVEL, RANK) 
 );
 
 
+INSERT INTO APALABRADOS.USER (NAME, USERNAME, AVATAR, EMAIL, PASSWORD, LEVEL) VALUES (
+	'Ana Lucero',
+	'ana',
+	'ana.jpg',
+	'ana@unex.es',
+	'1234',
+	'newbie'
+);
+
+
+INSERT INTO APALABRADOS.USER (NAME, USERNAME, AVATAR, EMAIL, PASSWORD, LEVEL) VALUES (
+	'Pedro Baez',
+	'pedro',
+	'pedro.jpg',
+	'pedro@unex.es',
+	'1234',
+	'master'
+);
+
+
+INSERT INTO APALABRADOS.USER (NAME, USERNAME, AVATAR, EMAIL, PASSWORD, LEVEL) VALUES (
+	'Sara Garcia',
+	'sara',
+	'sara.jpg',
+	'sara@unex.es',
+	'1234',
+	'advanced'
+);
+
+
+INSERT INTO APALABRADOS.USER (NAME, USERNAME, AVATAR, EMAIL, PASSWORD, LEVEL) VALUES (
+	'Daniel Gonzalez',
+	'daniel',
+	'daniel.jpg',
+	'daniel@unex.es',
+	'1234',
+	'newbie'
+);
+
+
+
+
+INSERT INTO APALABRADOS.GAME (STATUS, P1TURN, P1, P2, P1SCORE, P2SCORE, BOARD) VALUES (
+	'playing',
+	0,
+	1,
+	2,
+	10,
+	55,
+	'               .               .               .               .               .               .         C     .    ALVARO     .         Q     .         U     .         I     .               .               .               .               '
+);
+
+
 INSERT INTO APALABRADOS.GAME (STATUS, P1TURN, P1, P2, P1SCORE, P2SCORE, BOARD) VALUES (
 	'playing',
 	1,
-	1,
-	2,
+	3,
+	4,
 	13,
 	5,
 	'               .               .               .               .               .               .               .     PIUEX     .               .               .               .               .               .               .               '
 );
 
+
 INSERT INTO APALABRADOS.GAME (STATUS, P1TURN, P1, P2, P1SCORE, P2SCORE, BOARD) VALUES (
 	'initializing',
 	1,
-	2,
-	3,
+	4,
+	5,
 	0,
 	0,
 	'               .               .               .               .               .               .               .               .               .               .               .               .               .               .               '
 );
 
+
 INSERT INTO APALABRADOS.GAME (STATUS, P1TURN, P1, P2, P1SCORE, P2SCORE, BOARD) VALUES (
 	'finish',
 	0,
-	3,
-	4,
+	5,
+	6,
 	100,
 	94,
 	'               .               .               .               .               .               .               .  APALABRADOS  .               .               .               .               .               .               .               '
